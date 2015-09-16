b971739497c6a72c97d7931e744f72023c807fd4
damienChesneau <ches.damien@gmail.com>
2015-09-16 11:33:28 +0200

    Add a file chooser :)

--- src/fr/damienchesneau/icsformat/CalendarUpdater.java
+++ src/fr/damienchesneau/icsformat/CalendarUpdater.java
@@ -1,8 +1,10 @@
 package fr.damienchesneau.icsformat;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JFileChooser;
 
 /**
  *
@@ -11,16 +13,27 @@ import java.util.logging.Logger;
 public class CalendarUpdater {
 
     public static void main(String[] args) {
+        String inputFile = "", output = "";
         if (args.length == 2) {
-            IcsModifier modifier = new IcsModifier(args[0], args[1]);
-            try {
-                modifier.update();
-            } catch (IOException ex) {
-                System.err.println("Oooops error :(");
-                Logger.getLogger(CalendarUpdater.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }else{
-            System.out.println("Please use this app with tow parameters. First is input file the second is the output file.");
+            inputFile = args[0];
+            output = args[1];
+        } else {
+            //System.out.println("Please use this app with tow parameters. First is input file the second is the output file.");
+            JFileChooser fileToChoose = new JFileChooser();
+            fileToChoose.showDialog(null, null);
+            Path file = fileToChoose.getSelectedFile().toPath();
+            inputFile = file.toAbsolutePath().toString();
+            int index = inputFile.indexOf(".ics");
+            output = inputFile;
+            output = (String) output.subSequence(0, index);
+            output = output +"New.ics";
+        }
+        IcsModifier modifier = new IcsModifier(inputFile, output);
+        try {
+            modifier.update();
+        } catch (IOException ex) {
+            System.err.println("Oooops error :(");
+            Logger.getLogger(CalendarUpdater.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
